05615be8e416b98e70c66b844f575bf011666421
Paul Tyng <ptyng@underarmour.com>
2016-02-07 13:42:56 -0500

    Update for ES2015, log to Slack, use updated tagging

--- functions/tag-conformity/src/index.js
+++ functions/tag-conformity/src/index.js
@@ -1,86 +1,140 @@
+import 'babel-polyfill';
 import aws from 'aws-sdk';
-import async from 'async';
+import λ from 'apex.js'
 import moment from 'moment';
+import Bluebird from 'bluebird';
+import axios from 'axios';
+
+//update global promise implementation
+global.Promise = Bluebird;
+
+const region = new aws.Config().region;
 
 const ec2 = new aws.EC2({apiVersion: '2014-10-01'});
+Bluebird.promisifyAll(Object.getPrototypeOf(ec2));
+
 const defaultTimeToLive = moment.duration(4, 'hours');
+const tagPrefix = "ua";
+const requiredTags = {
+  "environment": /^(production|staging|development|integration)$/,
+  "product": /^(b2c|b2b)$/,
+  "role": /^.+$/,
+  "team": /^.+$/
+}
 
-export default function(event, context) {
-  function shouldStop(instance) {
-    var timeToLive = moment.duration(defaultTimeToLive.asMilliseconds());
-    var ttlFound = false;
+function tagKey(tag) {
+  return `${tagPrefix}:${tag}`;
+}
 
-    instance.Tags.forEach(function (tag) {
-      if (tag.Key == 'permanent') {
-        ttlFound = true;
-        return false;
-      } else if (tag.Key == "ttl-hours") {
-        ttlFound = true;
-        timeToLive = moment.duration(tag.Value, 'hours');
-      }
-    });
+function instanceTagMap(instance) {
+  return instance.Tags.reduce((map, tag) => {
+    map.set(tag.Key, tag.Value);
+    return map;
+  }, new Map());
+}
 
-    var upTime = new Date().getTime() - instance.LaunchTime.getTime();
+function validateTagMap(tagMap) {
+  const messages = []
 
-    if (upTime < timeToLive.asMilliseconds()) {
-      timeToLive.subtract(upTime);
-      console.log("Instance (" + instance.InstanceId + ") has " + timeToLive.humanize() + " remaining.");
-      return false;
-    } else if (!ttlFound) {
-      console.log("Instance (" + instance.InstanceId + ") has no TTL tag.");
+  // Check all required tags for existence and pattern match
+  for(let [key, pattern] of Object.entries(requiredTags)) {
+    key = tagKey(key);
+
+    if(!tagMap.has(key)) {
+      messages.push(`missing tag '${key}'`);
     }
-    return true;
+    else if(!pattern.test(tagMap.get(key))) {
+      messages.push(`malformed tag '${key}'`);
+    }
   }
 
-  async.waterfall([
-      function fetchEC2Instances(next) {
-        var ec2Params = {
-          Filters: [
-            {Name: 'instance-state-name', Values: ['running']}
-          ]
-        };
+  return messages;
+}
 
-        ec2.describeInstances(ec2Params, function (err, data) {
-          next(null, err, data)
-        });
-      },
-      function filterInstances(err, data, next) {
-        var stopList = [];
+function slackInstance(instance) {
+  const tagMap = instanceTagMap(instance);
+  const name = tagMap.get('Name');
 
-        data.Reservations.forEach(function (res) {
-          res.Instances.forEach(function (instance) {
-            if (shouldStop(instance)) {
-              stopList.push(instance.InstanceId);
-            }
-          });
-        });
-        next(null, stopList);
-      },
-      function stopInstances(stopList, next) {
-        if (stopList.length > 0) {
-          //ec2.stopInstances({InstanceIds: stopList}, function (err, data) {
-          //  if (err) {
-          //    nex(err);
-          //  }
-          //  else {
-          //    console.log(data);
-          //    next(null);
-          //  }
-          //});
-          next(null);
+  return `<https://console.aws.amazon.com/ec2/v2/home?region=${region}#Instances:instanceId=${instance.InstanceId};sort=instanceState|${name} _${instance.InstanceId}_>`;
+}
+
+export default λ((e, ctx) => {
+  //context.succeed(Object result);
+  //context.fail(Error error);
+  //context.done(Error error, Object result);
+  //context.getRemainingTimeInMillis();
+  //context.functionName
+  //context.functionVersion
+  //context.invokedFunctionArn
+  //context.memoryLimitInMB
+  //context.awsRequestId
+  //context.logGroupName
+  //context.logStreamname
+  //context.identity (Object)
+  //context.clientContext (Object)
+
+  const SLACK_WEBHOOK_URL = process.env.SLACK_WEBHOOK_URL;
+
+  if(SLACK_WEBHOOK_URL == null) {
+    throw new Error('SLACK_WEBHOOK_URL not set.');
+  }
+
+  return ec2.describeInstancesAsync({
+    Filters: [
+      { Name: 'instance-state-name', Values: ['running'] }
+    ]
+  })
+    .get('Reservations')
+    .reduce((acc, res) => acc.concat(res.Instances), [])
+    .map(instance => {
+      const tagMap = instanceTagMap(instance);
+      const errors = validateTagMap(tagMap);
+      const warnings = [];
+
+      let stop = true;
+
+      // Check TTL
+      if(tagMap.has(tagKey('permanent'))) {
+        stop = false;
+      }
+      else if(tagMap.has(tagKey('ttl-hours'))) {
+        const value = parseFloat(tagMap.get(tagKey('ttl-hours')));
+
+        if(Number.isNaN(value)) {
+          errors.push(`'${value}' is an invalid TTL.`);
         }
         else {
-          console.log("No instances need to be stopped");
-          next(null);
+          const ttl = moment.duration(tagMap.get(tagKey('ttl-hours')), 'hours');
+          const upTime = new Date().getTime() - instance.LaunchTime.getTime();
+
+          if (upTime < ttl.asMilliseconds()) {
+            ttl.subtract(uptime);
+            warnings.push(`${ttl.humanize()} remaining on TTL.`)
+            stop = false;
+          }
         }
       }
-    ],
-    function (err) {
-      if (err) {
-        console.error('Failed to clean EC2 instances: ', err);
-      } else {
-        console.log('Successfully cleaned all unused EC2 instances.');
+      else {
+        errors.push(`no TTL.`);
       }
-      context.done(err);
-    });
-};
+
+      return { id: instance.InstanceId, instance, warnings, errors, stop };
+    })
+    .then(instances => {
+      const attachments = [];
+
+      instances.forEach(({ id, instance, warnings, errors, stop }) => {
+        // TODO: lookup instance.RequesterId for shaming...
+        attachments.push(...warnings.map(m => ({ fallback: `Warning: '${id}' ${m}`, text: `*Warning:* ${slackInstance(instance)} ${m}`, color: 'warning', mrkdwn_in: ['text'] })));
+        attachments.push(...errors.map(m => ({ fallback: `Error: '${id}' ${m}`, text: `*Error:* ${slackInstance(instance)} ${m}`, color: 'danger', mrkdwn_in: ['text'] })));
+
+        if(stop) {
+          attachments.push({ fallback: `Stopping: '${id}'`, text: `*Stopping:* ${slackInstance(instance)}`, color: 'danger', mrkdwn_in: ['text'] });
+        }
+      });
+
+      return axios
+        .post(SLACK_WEBHOOK_URL, { text: 'Successfully reviewed tags of all instanced.', attachments });
+    })
+    .then(() => console.log('Successfully reviewd tags of all instances.'));
+});
